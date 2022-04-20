cf17acb99a4d4c035d4653b46905c4062f1dd39f
hoact99 <hoact.it@gmail.com>
2022-04-20 08:01:59 +0700

    create pages for admin

--- backend/layouts/partials/sidebar.php
+++ backend/layouts/partials/sidebar.php
@@ -24,6 +24,54 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
+        Administrations
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseAdmins" aria-expanded="true"
+            aria-controls="collapseAdmins">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Pages</span>
+        </a>
+        <div id="collapseAdmins" class="collapse" aria-labelledby="headingAdmins" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Employee</h6>
+                <a class="collapse-item" href="../manage/brands.php">Brands</a>
+                <a class="collapse-item" href="../manage/images.php">Images</a>
+                <a class="collapse-item" href="../manage/parent_categories.php">Parent Categories</a>
+                <a class="collapse-item" href="../manage/payments.php">Payments</a>
+                <a class="collapse-item" href="../manage/permissions.php">Permissions</a>
+                <a class="collapse-item" href="../manage/positions.php">Positions</a>
+                <a class="collapse-item" href="../manage/order_status.php">Order Status</a>
+                <a class="collapse-item" href="../manage/specifications.php">Specifications</a>
+                <a class="collapse-item" href="../manage/accounts.php">Accounts</a>
+                <a class="collapse-item" href="../manage/persons.php">Persons</a>
+                <a class="collapse-item" href="../manage/suppliers.php">Suppliers</a>
+                <a class="collapse-item" href="../manage/customers.php">Customers</a>
+                <a class="collapse-item" href="../manage/shippers.php">Shippers</a>
+                <a class="collapse-item" href="../manage/employees.php">Employees</a>
+                <a class="collapse-item" href="../manage/categories.php">Categories</a>
+                <a class="collapse-item" href="../manage/products.php">Products</a>
+                <a class="collapse-item" href="../manage/product_images.php">Product Images</a>
+                <a class="collapse-item" href="../manage/product_specifications.php">Product Specifications</a>
+                <a class="collapse-item" href="../manage/comments.php">Comments</a>
+                <a class="collapse-item" href="../manage/comment_images.php">Comment Images</a>
+                <a class="collapse-item" href="../manage/orders.php">Orders</a>
+                <a class="collapse-item" href="../manage/order_details.php">Order Details</a>
+                <div class="collapse-divider"></div>
+                <h6 class="collapse-header">Product</h6>
+                <a class="collapse-item" href="../admin/404.php">404 Page</a>
+                <a class="collapse-item" href="../admin/blank.php">Blank Page</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
         Interface
     </div>
 
