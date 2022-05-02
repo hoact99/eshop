29b1e575b36ea1f96b020c78f2abe2a705836035
ThangLe <lnvthang.it@gmail.com>
2022-05-02 16:51:24 +0700

    Cache, Cookie, Local Storage & Session Storage documents and have code to present

---
+++ documents/savedataonweb/sessionstorage/sessionstorage_script.js
@@ -0,0 +1,16 @@
+// Create sesionStorage
+sessionStorage.setItem("session", "Session storage");
+
+// Get sessionStorage
+var sessionStorage = sessionStorage.getItem("session");
+console.log(sessionStorage);
+
+// Length
+var sessionStorageLength = sessionStorage.length;
+console.log(sessionStorageLength);
+
+// Delete 1 item localStorage
+// sessionStorage.removeItem('name');
+
+// Clear item in sessionStorage
+// sessionStorage.clear();
