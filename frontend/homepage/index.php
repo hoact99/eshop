e64fc04090379b9c30254cd67f0d1052747d37ed
ThangLe <lnvthang.it@gmail.com>
2022-05-03 21:43:08 +0700

    Comment code

---
+++ frontend/homepage/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <div class="nav">
+            <ul class="nav-list">
+                <li class="nav-item">Giới thiệu</li>
+                <li class="nav-item">Kết nối</li>
+                <li class="nav-item">Tải ứng dụng</li>
+            </ul>
+            <ul class="nav-list">
+                <li class="nav-item">Đăng ký</li>
+                <li class="nav-item">Đăng nhập</li>
+            </ul>
+        </div>
+    </div>
+    <div class="content">
+
+        <div class="column-layout">
+            <div class="column-item sidebar-one">
+                <h1>Sidebar 1</h1>
+            </div>
+            <div class="column-item main-column">
+                <h1>Sidebar main</h1>
+            </div>
+            <div class="column-item sidebar-two">
+                <h1>Sidebar 2</h1>
+            </div>
+        </div>
+
+        <div class="center-layout">
+            <h3 class="center-layout__item">
+                Center item
+            </h3>
+        </div>
+
+        <div class="wrap-layout">
+            <div class="wrap-layout__item">1</div>
+            <div class="wrap-layout__item">2</div>
+            <div class="wrap-layout__item">3</div>
+            <div class="wrap-layout__item">4</div>
+            <div class="wrap-layout__item">5</div>
+        </div>
+
+    </div>
+    <div class="footer-layout">
+        <div class="footer-item">
+            <h2>Footer 1</h2>
+        </div>
+        <div class="footer-item">
+            <h2>Footer 2</h2>
+        </div>
+        <div class="footer-item">
+            <h2>Footer 3</h2>
+        </div>
+    </div>
+</body>
+</html>
