de92379478e7da4dc306565ee8522850d1497081
ThangLe <lnvthang.it@gmail.com>
2022-05-10 23:27:24 +0700

    Created: index.php and style.css from flexbox of F8

--- frontend/homepage/index.php
+++ frontend/homepage/index.php
@@ -1,19 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home page</title>
+
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="header">
         <div class="nav">
             <ul class="nav-list">
                 <li class="nav-item">Giới thiệu</li>
                 <li class="nav-item">Kết nối</li>
-                <li class="nav-item">Tải ứng dụng</li>
+                <li class="nav-item">
+                    <button class="btn btn-primary">Facebook</button>
+                </li>
+                <li class="nav-item">
+                    <button class="btn btn-danger">Gmail</button>
+                </li>
+            </ul>
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <form action="/action_page.php">
+                        <input type="search" id="search" name="search">
+                        <input type="submit">
+                    </form>
+                </li>
             </ul>
             <ul class="nav-list">
                 <li class="nav-item">Đăng ký</li>
@@ -21,17 +37,29 @@
             </ul>
         </div>
     </div>
+
+
+
+
     <div class="content">
+
+        <div class="wrap-layout">
+            <div class="wrap-layout__item">Fashion</div>
+            <div class="wrap-layout__item">Electron</div>
+            <div class="wrap-layout__item">House</div>
+            <div class="wrap-layout__item">Car</div>
+            <div class="wrap-layout__item">Food</div>
+        </div>
 
         <div class="column-layout">
             <div class="column-item sidebar-one">
-                <h1>Sidebar 1</h1>
+                <h1>Category</h1>
             </div>
             <div class="column-item main-column">
-                <h1>Sidebar main</h1>
+                <h1>Content</h1>
             </div>
             <div class="column-item sidebar-two">
-                <h1>Sidebar 2</h1>
+                <h1>Ads</h1>
             </div>
         </div>
 
@@ -41,13 +69,7 @@
             </h3>
         </div>
 
-        <div class="wrap-layout">
-            <div class="wrap-layout__item">1</div>
-            <div class="wrap-layout__item">2</div>
-            <div class="wrap-layout__item">3</div>
-            <div class="wrap-layout__item">4</div>
-            <div class="wrap-layout__item">5</div>
-        </div>
+
 
     </div>
     <div class="footer-layout">
@@ -62,4 +84,5 @@
         </div>
     </div>
 </body>
+
 </html>
